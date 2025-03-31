0b3db275441d260f97fc92910db7484304c67f77
jeqcho <jeqin_chooi@college.harvard.edu>
2025-03-31 17:36:09 -0400

    reduced size by moving jsons to assets

--- README.md
+++ README.md
@@ -14,8 +14,7 @@ We use Angular CLI, so make sure to download it beforehand.
 1. Create a new component by running in terminal `ng generate component Fall2025`. Check if it is now imported at `app.module.ts`.
 2. Edit `src/app/app-routing.module.ts` to include the new component and to edit the default path.
 3. Edit `src/app/archive/archive.component.ts`.
-4. Copy over the output folder from the scraping repo (e.g. `2025_Fall_2024_Fall`) to the folder of the component and rename it to just `data`.
-4. Move the `departments` folder from the folder that you just copied into a new folder under `assets/data/` named for the term you are building for.
+4. Make a new folder under `assets/data/` with the appriopriate file name and over the contents of the output folder from the scraping repo (e.g. the folder `2025_Fall_2024_Fall` which contains `super_gems.json` and the `departments` folder).
 5. Copy over the `???.component.ts` from another recent component and make necessary changes. You need to handpick the featured ~5 Super Gems, Hidden Gems, and Great Courses from `course_ratings_analysis.ipynb` in the scraping repo (look for the cells that present to you a candidate list of each). Also edit the my.harvard search term.
 6. Copy over the `???.component.html` from another recent component and make necessary changes. Remember to change the number of classes near the top, and the link at the bottom. Simply count the number of rows in `release/myharvard/YEAR_TERM.csv` in the scraping repo.
 7. Run `ng serve` to test it out.
@@ -31,6 +30,5 @@ Contributions are welcome! Please open a pull request.
 
 - FIX: Checking [https://hugems.net/archive] on a HTTP status checker like [https://httpstatus.io/] shows that the webpage returns 404, though it renders the archive page correctly.
 - IMPROVEMENT: Since this project was completed and since maintained before the LLMs took off, it might be helpful to do a scan on Cursor to identify possible bugs and improvement suggestions, as well as cleaning up code and making it more readable.
-- Production size is big at >2MB
 - Make footer consistent across all pages
 - Fix Expos 20 for Fall 2025
--- src/app/fall2025/fall2025.component.ts
+++ src/app/fall2025/fall2025.component.ts
@@ -1,8 +1,4 @@
-import { Component } from '@angular/core';
-import super_gems from "./data/super_gems.json";
-import great_courses from "./data/great_courses.json";
-import department_list from "./data/department_list.json"
-import hidden_gems from "./data/hidden_gems.json"
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -10,92 +6,96 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './fall2025.component.html',
   styleUrls: ['./fall2025.component.scss']
 })
-export class Fall2025Component {
+export class Fall2025Component implements OnInit {
   dropDownList: any;
-  constructor(private http: HttpClient) { }
-
-  super_gems = super_gems;
-  great_courses = great_courses;
-  hidden_gems = hidden_gems;
-  full_department_list = department_list;
+  super_gems: any;
+  great_courses: any;
+  department_list: any;
+  hidden_gems: any;
+  full_department_list: any;
   special_list = [
     "General Education",
     "First Year Seminar Program",
   ]
-
-  partial_department_list = department_list;
-  selected_great_courses = great_courses;
-  selected_super_gems = super_gems;
-  selected_hidden_gems = hidden_gems;
+  partial_department_list: any;
+  selected_great_courses: any;
+  selected_super_gems: any;
+  selected_hidden_gems: any;
   expos: any;
-
-
   department_gems: Record<string, any> = {}
   BigString = String
-
   term_code ="2258"// get this 4-digit by going to my.harvard and use the advanced search
   term = "(Term%3A%5C%22" + this.term_code + "%5C%22)"
-
   url_prefix = 'https://portal.my.harvard.edu/psp/hrvihprd/EMPLOYEE/EMPL/h/?tab=HU_CLASS_SEARCH&SearchReqJSON=%7B%22SearchText%22%3A%22'
-
   url_suffix = '%22%7D';
 
-  getDataHttp(url: string) {
+  constructor(private http: HttpClient) { }
 
+  getDataHttp(url: string) {
     return this.http.get(url);
   }
 
-  ngOnInit(): void {
-    for (let spec_department of this.special_list) {
-      this.partial_department_list = this.partial_department_list.filter(x => x !== spec_department);
-    }
+  ngOnInit() {
+    // Load main data files
+    this.http.get('/assets/data/spring2025/super_gems.json').subscribe(data => {
+      this.super_gems = data;
+      this.selected_super_gems = this.super_gems.filter((x: any) => [
+        'EMR 1010',
+        "HEB 123",
+        "PSY 1813",
+        "GENED 1025",
+        "PHIL 9",
+      ].includes(x.course_code));
+    });
 
-    // handpick hidden gems
-    // get from matching course indexes
+    this.http.get('/assets/data/spring2025/great_courses.json').subscribe(data => {
+      this.great_courses = data;
+      this.selected_great_courses = this.great_courses.filter((x: any) => [
+        "AFRAMER 133Y",
+        "GOV 1796",
+        "ECON 2725",
+        "RELIGION 32",
+        "EXPOS 40"
+      ].includes(x.course_code));
+    });
 
-    this.selected_super_gems = this.super_gems.filter((x: any) => [
-      'EMR 1010',
-      "HEB 123",
-      "PSY 1813",
-      "GENED 1025",
-      "PHIL 9",
-    ].includes(x.course_code));
-
-    this.selected_hidden_gems = this.hidden_gems.filter((x: any) => [
-      "HEB 30",
-      "CLS-STDY 166",
-      "TDM 121MV",
-      "SOCIOL 1129",
-      "GHHP 50",
-    ].includes(x.course_code));
-
-    // remove duplicate codes by course_code
-    this.selected_hidden_gems = this.selected_hidden_gems.filter((x: any, index: number, self: any) =>
-      index === self.findIndex((t: any) => (
-        t.course_code === x.course_code
-      ))
-    )
-
-    this.selected_great_courses = this.great_courses.filter((x: any) => [
-      "AFRAMER 133Y",
-      "GOV 1796",
-      "ECON 2725",
-      "RELIGION 32",
-      "EXPOS 40"
-    ].includes(x.course_code));
-
-    this.special_list = this.special_list.filter(x => x !== 'Expository Writing');
-
-    for (let department of department_list) {
-      this.getDataHttp(`assets/data/fall2025/departments/${department}.json`).subscribe(
-        (data: any) => {
-          this.department_gems[department] = data;
-          if (department == "Expository Writing") {
-            this.expos = this.department_gems['Expository Writing'].filter((x: any) => x.course_code != "EXPOS 40");
+    this.http.get('/assets/data/spring2025/department_list.json').subscribe(data => {
+      this.department_list = data;
+      this.full_department_list = data;
+      this.partial_department_list = data;
+      for (let spec_department of this.special_list) {
+        this.partial_department_list = this.partial_department_list.filter((x: string) => x !== spec_department);
+      }
+      this.special_list = this.special_list.filter((x: string) => x !== 'Expository Writing');
+      for (let department of this.department_list) {
+        this.getDataHttp(`assets/data/fall2025/departments/${department}.json`).subscribe(
+          (data: any) => {
+            this.department_gems[department] = data;
+            if (department == "Expository Writing") {
+              this.expos = this.department_gems['Expository Writing'].filter((x: any) => x.course_code != "EXPOS 40");
+            }
           }
-        }
+        )
+      }
+    });
+
+    this.http.get('/assets/data/spring2025/hidden_gems.json').subscribe(data => {
+      this.hidden_gems = data;
+      this.selected_hidden_gems = this.hidden_gems.filter((x: any) => [
+        "HEB 30",
+        "CLS-STDY 166",
+        "TDM 121MV",
+        "SOCIOL 1129",
+        "GHHP 50",
+      ].includes(x.course_code));
+
+      // remove duplicate codes by course_code
+      this.selected_hidden_gems = this.selected_hidden_gems.filter((x: any, index: number, self: any) =>
+        index === self.findIndex((t: any) => (
+          t.course_code === x.course_code
+        ))
       )
-    }
+    });
   }
 }
 
