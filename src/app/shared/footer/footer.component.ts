c66f3a359cd86b41552af2279119f022015770aa
jeqcho <jeqin_chooi@college.harvard.edu>
2025-03-31 18:40:36 -0400

    auto footer year edit

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  startYear = 2022;
+  currentYear = new Date().getFullYear();
+  yearRange = `${this.startYear}-${this.currentYear}`;
 }
